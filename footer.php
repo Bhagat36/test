4752e0f66e130fcf02eb9659f1b4a1823ab69e07
Bhagat36 <39501554+Bhagat36@users.noreply.github.com>
2018-05-22 11:45:34 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php
+                        dynamic_sidebar('footer-part');
+                        ?>
+    <!-- Footer -->
+   <!-- <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; MYcart 2018</p>
+      </div>
+      <!-- /.container -->
+    
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php blogInfo('stylesheet_directory'); ?>/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php blogInfo('stylesheet_directory'); ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
